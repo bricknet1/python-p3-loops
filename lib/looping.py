a57154c08df2c2f339a8bbb9889d7fefa825efca
Nick Johnson <nlj222@gmail.com>
2023-02-21 11:04:37 -0800

    lab complete

--- lib/looping.py
+++ lib/looping.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    for i in range(10, 0, -1):
+    for i in range(10,0,-1):
         print(i)
-    
     print("Happy New Year!")
 
 def square_integers(int_list):
     return [i ** 2 for i in int_list]
 
 def fizzbuzz():
-    for i in range(1, 101):
-        if not i % 15:
+    for i in range(1,101):
+        if (((i/3).is_integer()) and ((i/5).is_integer())):
             print("FizzBuzz")
-        elif not i % 5:
+        elif (i/5).is_integer():
             print("Buzz")
-        elif not i % 3:
+        elif (i/3).is_integer():
             print("Fizz")
         else:
             print(i)
